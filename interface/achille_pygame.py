5110b41439a1a94796a7fb6ba96d4a3c1bb3dc1f
Hippo <hippolyte.geslain@laplateforme.io>
2025-09-04 11:11:39 +0200

    Added sliders in Achille.py

--- interface/achille_pygame.py
+++ interface/achille_pygame.py
@@ -1,5 +1,10 @@
+from random import choice
+import time
 import pygame
 import sys
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 
 # Simulation parameters
 speed_achille = 10
@@ -24,7 +29,7 @@ def move_ach_to_tort(position_achille, position_tortoise, speed_achille, speed_t
     total_time += time_spent
     return position_achille, position_tortoise, iteration, total_time
 
-def race(position_achille, speed_achille, position_tortoise, speed_tortoise, iteration, total_time):
+def race(position_achille, position_tortoise,speed_achille, speed_tortoise, iteration, total_time):
     position_achille += speed_achille
     position_tortoise += speed_tortoise
     iteration += 1
@@ -38,39 +43,85 @@ def main_achille():
     pygame.display.set_caption("Zeno Paradox - Achille and the Tortoise")
     mode = None
 
+    sliderTurtlePos = Slider(screen, 230, 120, 400, 40, min=10, max=99, step=1, initial=10)
+    outputTurtlePos = TextBox(screen, 680, 115, 80, 50, fontSize=30)
+    outputTurtlePos.disable()
+
+    sliderTurtleSpeed = Slider(screen, 230, 220, 400, 40, min=0, max=100, step=1,initial=5)
+    outputTurtleSpeed = TextBox(screen, 680, 215, 80, 50, fontSize=30)
+    outputTurtleSpeed.disable()
+
+    sliderAchilleSpeed = Slider(screen, 230, 320, 400, 40, min=0, max=100, step=1,initial=10)
+    outputAchilleSpeed = TextBox(screen, 680, 315, 80, 50, fontSize=30)
+    outputAchilleSpeed.disable()
+
+
     font = pygame.font.SysFont(None, 24)
     clock = pygame.time.Clock()
+
+    conditionmsg = font.render("Keep in mind that Achille IS faster than the turtle",True,(255,0,0))
 
     choosing = True
 
     while choosing:
-        for event in pygame.event.get():
+    
+        events=pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    mode = 1
-                    choosing =False
+                    if sliderAchilleSpeed.getValue()>sliderTurtleSpeed.getValue():    
+                        mode = 1
+                        choosing =False
+                    else:
+                        screen.blit(conditionmsg,(210,80))
+                        pygame.display.flip()
+                        time.sleep(2)
+                        clock.tick(30)
                 if event.key == pygame.K_b:
-                    mode = 2
-                    choosing =False
+                    if sliderAchilleSpeed.getValue()>sliderTurtleSpeed.getValue():    
+                        mode = 2
+                        choosing =False
+                    else:
+                        screen.blit(conditionmsg,(210,80))
+                        pygame.display.flip()
+                        time.sleep(2)
+                        clock.tick(30)
+
 
         screen.fill(WHITE)
-        msg = font.render("Press a for Zeno Paradox, Press b for the normal race",True,BLACK)
-        screen.blit(msg, (50, HEIGHT//2 - 20))
+        turtleposmsg = font.render("Turtle Position",True,BLACK)
+        turtlespeedmsg = font.render("Turtle Speed",True, BLACK)
+        achillespeedmsg = font.render("Achille Speed",True, BLACK)
+        slidermsg = font.render("Try to change the parameters of the simulation",True,BLACK)
+        choicemsg = font.render("Press a for Zeno Paradox, Press b for the normal race",True,BLACK)
+        screen.blit(slidermsg, (200, 50))
+        screen.blit(choicemsg, (200, HEIGHT//2 + 220))
+        screen.blit(turtleposmsg,(50,133))
+        screen.blit(turtlespeedmsg,(50,233))
+        screen.blit(achillespeedmsg,(50,333))
+
+        
+        # Draw Sliders
+        outputTurtlePos.setText(str(sliderTurtlePos.getValue()))
+        outputTurtleSpeed.setText(str(sliderTurtleSpeed.getValue()))
+        outputAchilleSpeed.setText(str(sliderAchilleSpeed.getValue()))
+        pygame_widgets.update(events)
+
         pygame.display.flip()
         clock.tick(30)
 
     running = True
     achille_x = position_achille
-    tortoise_x = position_tortoise
+    tortoise_x = sliderTurtlePos.getValue()
     iteration = 0
     total_time = 0
 
     while running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
@@ -91,9 +142,11 @@ def main_achille():
         tortoise_label = font.render(f"Tortoise: {tortoise_x:.2f}m", True, BLACK)
         screen.blit(tortoise_label, (int(tortoise_x)+30, HEIGHT//2+90))
 
-        # Draw info
+        # Draw Info
         info = font.render(f"Iteration: {iteration}  Total time: {total_time:.2f}s  Mode: {mode}", True, BLACK)
         screen.blit(info, (10, 10))
+
+
 
         pygame.display.flip()
         clock.tick(1)  # Slow down for visualization
@@ -102,12 +155,14 @@ def main_achille():
         if achille_x < tortoise_x:
             if mode == 1:
                 achille_x, tortoise_x, iteration, total_time = move_ach_to_tort(
-                    achille_x, tortoise_x, speed_achille, speed_tortoise, iteration, total_time
+                    achille_x, sliderTurtlePos.getValue(), sliderAchilleSpeed.getValue(), sliderTurtleSpeed.getValue(), iteration, total_time
                 )
             elif mode == 2:
                 achille_x, tortoise_x, iteration, total_time = race(
-                    achille_x, speed_achille, tortoise_x, speed_tortoise, iteration, total_time
+                    achille_x,sliderTurtlePos.getValue(),sliderAchilleSpeed.getValue(),sliderTurtleSpeed.getValue(), iteration, total_time
                 )
+            print(f"pos T = {sliderTurtlePos.getValue()}\nspeed T {sliderTurtleSpeed.getValue()}\nspeed A {sliderAchilleSpeed.getValue()}") 
+
         else:
             end_text = font.render("Achille has caught back the Tortoise!", True, (200, 0, 0))
             screen.blit(end_text, (WIDTH//2-100, HEIGHT//2-10))
